970b6207aedfe759e9941c2a41fe3f68272974c5
b.husainov <bulatkhusainov@yandex.ru>
2015-08-04 13:56:09 +0300

    mobile version (320px) without datepickers

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,6 +3,8 @@
 "use strict";
 
 $(function () {
+	// mobile menu
+
 	$(".mobile-menu-open").on("click", function() {
 		$(".mobile-menu").addClass("active");
 	})
@@ -10,4 +12,25 @@ $(function () {
 	$(".mobile-menu-close").on("click", function() {
 		$(".mobile-menu").removeClass("active");
 	})
+
+	// map
+
+    function initialize() {
+        var mapOptions = {
+            center: {
+                lat: -34.397,
+                lng: 150.644
+            },
+            zoom: 8,
+            scrollwheel: false,
+            draggable: false,
+            disableDoubleClickZoom: true,
+            disableDefaultUI: true,
+        };
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
